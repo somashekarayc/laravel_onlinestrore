35beee56eb31457f473cbdd945de3e215bcc27c5
som <somashekarayc@gmail.com>
2023-11-23 01:18:40 +0530

    six

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="row">
 <div class="col-md-6 col-lg-4 mb-2">
-<img src="https://github.com/PracticalBooks/Practical-Laravel/blob/main/Chapter06/onlineStore/public/img/game.png" class="img-fluid rounded">
+<img src="{{ asset('/img/four.jpg') }}" class="img-fluid rounded">
 </div>
 <div class="col-md-6 col-lg-4 mb-2">
-<img src="https://github.com/PracticalBooks/Practical-Laravel/blob/main/Chapter06/onlineStore/public/img/safe.png" class="img-fluid rounded">
+<img src="{{ asset('/img/five.jpg') }}"  class="img-fluid rounded">
 </div>
 <div class="col-md-6 col-lg-4 mb-2">
-<img src="https://github.com/PracticalBooks/Practical-Laravel/blob/main/Chapter06/onlineStore/public/img/submarine.png" class="img-fluid rounded">
+<img src="{{ asset('/img/six.jpg') }}"  class="img-fluid rounded">
 </div>
 </div>
 @endsection
