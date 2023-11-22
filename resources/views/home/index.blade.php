53ac975fe588ada3cfec60620efbfea879b75a26
som <somashekarayc@gmail.com>
2023-11-22 11:27:54 +0530

    ini

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('title', $viewData["title"])
+@section('content')
+<div class="row">
+<div class="col-md-6 col-lg-4 mb-2">
+<img src="https://github.com/PracticalBooks/Practical-Laravel/blob/main/Chapter06/onlineStore/public/img/game.png" class="img-fluid rounded">
+</div>
+<div class="col-md-6 col-lg-4 mb-2">
+<img src="https://github.com/PracticalBooks/Practical-Laravel/blob/main/Chapter06/onlineStore/public/img/safe.png" class="img-fluid rounded">
+</div>
+<div class="col-md-6 col-lg-4 mb-2">
+<img src="https://github.com/PracticalBooks/Practical-Laravel/blob/main/Chapter06/onlineStore/public/img/submarine.png" class="img-fluid rounded">
+</div>
+</div>
+@endsection
