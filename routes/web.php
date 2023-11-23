020a43ab73185f9644d1e58b59beb99f747a51a9
som <somashekarayc@gmail.com>
2023-11-23 15:20:31 +0530

    eight

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// |--------------------------------------------------------------------------
 
 Route::get('/', [HomeController::class, 'index'])->name("home.index");
 Route::get('/about', [HomeController::class, 'about'])->name("home.about");
+
+// |--------------------------------------------------------------------------
+
+Route::get('/products', [ProductController::class, 'index'])->name("product.index");
+Route::get('/products/{id}', [ProductController::class, 'show'])->name("product.show");
