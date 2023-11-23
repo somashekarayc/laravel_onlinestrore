26d76df9325f5307b145a7e94168ec12c7e5c9f6
som <somashekarayc@gmail.com>
2023-11-23 09:52:15 +0530

    seven

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App \ Http \ Controllers \ HomeController@index')->name("home.index");
-
-Route::get('/about', 'App \ Http \ Controllers \ HomeController@about')->name("home.about");
+Route::get('/', [HomeController::class, 'index'])->name("home.index");
+Route::get('/about', [HomeController::class, 'about'])->name("home.about");
