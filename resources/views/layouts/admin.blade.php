0f6d16db4673cd740bec90eade8dcc30bae522ef
som <somashekarayc@gmail.com>
2023-11-28 16:52:39 +0530

    fourteen

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,89 +5,74 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 
     <link href="{{ asset('/css/admin.css') }}" rel="stylesheet" />
     <title>@yield('title', 'Admin - Online Store')</title>
 </head>
 
 <body>
-    {{-- <div class="row g-0">
-        <!-- sidebar -->
-        <div class="p-3 col fixed text-white bg-dark">
-            <a href="{{ route('admin.home.index') }}" class="text-white text-decoration-none">
-                <span class="fs-4">Admin Panel</span>
-            </a>
-            <hr />
-            <ul class="nav flex-column">
-                <li><a href="{{ route('admin.home.index') }}" class="nav-link text-white">- Admin - Home</a></li>
-                <li><a href="{{ route('admin.product.index') }}" class="nav-link text-white">- Admin - Products</a></li>
-                <li>
-                    <a href="{{ route('home.index') }}" class="mt-2 btn bg-primary text-white">Go back to the home page</a>
-                </li>
-            </ul>
-        </div>
-        <!-- sidebar -->
-        <div class="col content-grey">
-            <nav class="p-3 shadow text-end">
-                <span class="profile-font">Admin</span>
-                <img class="img-profile rounded-circle" src="{{ asset('/img/undraw_profile.svg') }}">
-            </nav>
-            <div class="g-0 m-5">
-                @yield('content')
-            </div>
-        </div>
-    </div> --}}
-    {{-- ======================================================================================================== --}}
-    <div class="row g-0">
+    <button class="btn btn-primary m-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Menu</button>
 
-    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel"></h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: auto;">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-          <span class="fs-4">Admin Panel</span>
+            <span class="fs-4">Admin Panel</span>
         </a>
         <hr>
         <ul class="nav nav-pills flex-column mb-auto">
-          <li class="nav-item">
-            <a href="{{ route('admin.home.index') }}" class="nav-link active" aria-current="page">
-              Admin - Home
-            </a>
-          </li>
-          <li>
-            <a href="{{ route('admin.product.index') }}" class="nav-link link-dark">
-              Admin - Products
-            </a>
-          </li>
-          <li>
-            <a href="{{ route('home.index') }}" class="nav-link link-dark">
-              back to the home page
-            </a>
-          </li>
+            <li class="nav-item">
+                <a href="{{ route('admin.home.index') }}" class="nav-link" aria-current="page">
+                    Admin - Home
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('admin.product.index') }}" class="nav-link">
+                    Admin - Products
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('home.index') }}" class="nav-link">
+                    back to the home page
+                </a>
+            </li>
 
         </ul>
         <hr>
         <div class="dropdown">
-          <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-            <strong>mdo</strong>
-          </a>
-          <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
-          </ul>
+            <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
+                {{-- <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2"> --}}
+                <strong>Amin</strong>
+            </a>
+            <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                {{-- <li><a class="dropdown-item" href="#">New project...</a></li> --}}
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><a class="dropdown-item" href="#">Profile</a></li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Sign out</a></li>
+            </ul>
         </div>
-      </div>
-      <div class="col content-grey">
-        <nav class="p-3 shadow text-end">
+    </div>
+  </div>
+</div>
+
+    <div class="col content-grey">
+        {{-- <nav class="p-3 shadow text-end">
             <span class="profile-font">Admin</span>
             <img class="img-profile rounded-circle" src="{{ asset('/img/undraw_profile.svg') }}">
-        </nav>
+        </nav> --}}
         <div class="g-0 m-5">
             @yield('content')
         </div>
     </div>
-      </div>
+    </div>
 
     {{-- ======================================================================================================== --}}
 
--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,7 @@ Route::get('/admin', [AdminHomeController::class, 'index'])->name("admin.home.in
 Route::get('/admin/products', [AdminProductController::class, 'index'])->name("admin.product.index");
 Route::post('/admin/products/store', [AdminProductController::class, 'store'])->name("admin.product.store");
 Route::delete('/admin/products/{id}/delete', [AdminProductController::class, 'delete'])->name("admin.product.delete");
+Route::get('/admin/products/{id}/edit', [AdminProductController::class, 'edit'])->name("admin.product.edit");
+Route::put('/admin/products/{id}/update', [AdminProductController::class, 'update'])->name("admin.product.update");
 
 
