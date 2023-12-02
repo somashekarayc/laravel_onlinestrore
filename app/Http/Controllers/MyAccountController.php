89f90ec9f658d3c2ad941480279c74f724d95b90
som <somashekarayc@gmail.com>
2023-12-02 13:36:58 +0530

    eighteen 2

--- app/Http/Controllers/MyAccountController.php
+++ app/Http/Controllers/MyAccountController.php
@@ -9,12 +9,11 @@ use Illuminate\Support\Facades\Auth;
 class MyAccountController extends Controller
 {
     public function orders()
-{
-$viewData = [];
-$viewData["title"] = "My Orders - Online Store";
-$viewData["subtitle"] = "My Orders";
-viewData["orders"] = Order::with(['items.product'])->where('user_id', Auth::user()->getId())->get();
-return view('myaccount.orders')->with("viewData", $viewData);
-}
-
+    {
+        $viewData = [];
+        $viewData["title"] = "My Orders - Online Store";
+        $viewData["subtitle"] = "My Orders";
+        viewData["orders"] = Order::with(['items.product'])->where('user_id', Auth::user()->getId())->get();
+        return view('myaccount.orders')->with("viewData", $viewData);
+    }
 }
